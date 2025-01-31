9212f4f0965b6476a92c3a8ab2acd9a88689f09c
Jitesh Kumar <105545377+jitesh-20@users.noreply.github.com>
2025-01-31 19:26:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function highlight() {
-    //Write your code here
-
-
+ let boldWords = document.querySelectorAll("p strong");
+    boldWords.forEach(word => {
+        word.style.color = "green";
+    });
 }
 
 
 function return_normal() {
-    //Write your code here
-
-    
+ let boldWords = document.querySelectorAll("p strong");
+    boldWords.forEach(word => {
+        word.style.color = "black";
+    });
 }
